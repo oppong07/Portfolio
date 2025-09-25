10a522fd93519ce8a9dad4a986c684f1a75a920d
Emmanuel Oppong <eoamakes7@gmail.com>
2025-09-25 04:23:51 -0500

    updated
    updated

---
+++ components/Social.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter, FaInstagram} from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "https://github.com/emmaeng680" },
+  { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/emmanuel-oppong-612bb41b6/" },
+  { icon: <FaTwitter />, path: "" },
+  { icon: <FaInstagram />, path: "" },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
